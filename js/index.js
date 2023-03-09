203cc23a7bb6d8cfbc1d32803ea9a0f8f4f37bde
jonatasts <jonatassantos96@hotmail.com>
2023-03-09 14:03:00 -0300

    ✨ Load recent orders by javascript

--- js/index.js
+++ js/index.js
@@ -20,3 +20,27 @@ themeToggler.addEventListener("click", () => {
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
+
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const shippingColor =
+    order.shipping === "Recusado"
+      ? "danger"
+      : order.shipping === "Pendente"
+      ? "warning"
+      : "primary";
+  const trContent = `
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${shippingColor}">${order.shipping}</td>
+    <td class="info">
+      <a href="#">
+        <span class="material-icons-sharp">info</span>
+      </a>
+    </td>
+  `;
+  
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
